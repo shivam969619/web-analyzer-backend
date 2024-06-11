6c66015503ac3ff36bd519c839e44267c4643d80
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-06-11 18:42:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,8 +58,9 @@ app.post("/api/audit", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 function analyzeResults(results) {
